a5ec13995545b1598e77b2bd748413daa2f1d139
LEONARDO AUGUSTO ALBUQUERQUE <96493737+LeoProStyle@users.noreply.github.com>
2025-03-18 08:05:33 -0300

    useState
    ok

---
+++ layout/React-2025/4_eventos/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import EventHandlingExamples from './components/EventHandlingExamples'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+<EventHandlingExamples/>
+        
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ layout/React-2025/4_eventos/src/components/EventHandlingExamples.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"; 
+
+const EventHandlingExamples = () => {
+
+    const handleClick = () => {
+        alert('Testando');
+    }
+
+
+    const handleGreet = (name) => {
+alert ( `Olá ${name}`);
+    }
+
+
+
+    const [name, setName] = useState("");
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        alert(`Enviado! ${name}`);
+    }
+    return(
+        <div>
+<button onClick={() => alert("oi")}>Clique aqui</button>
+<button onClick={handleClick}>Clique aqui</button>
+<br></br>
+<button onClick={() => handleGreet ("Leo")}>dizer olá leo</button>
+<button onClick={() => handleGreet ("Thamiris")}>dizer olá thamiris</button>
+<br></br>
+<form onSubmit={handleSubmit}>
+    <input type="text"
+     value={name}
+      onChange={(e) => setName(e.target.value)}
+        placeholder="Digite seu nome"/>
+    <input type="submit" value="Enviar"/>
+</form>
+
+
+ </div>
+    )
+}
+
+export default EventHandlingExamples;
