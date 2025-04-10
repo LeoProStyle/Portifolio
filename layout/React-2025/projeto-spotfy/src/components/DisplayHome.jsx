206a048cb9ba0262fced45413d4ffa43a28af366
LEONARDO AUGUSTO ALBUQUERQUE <96493737+LeoProStyle@users.noreply.github.com>
2025-04-10 16:36:16 -0300

    ajustes player
    ok

--- layout/React-2025/projeto-spotfy/src/components/DisplayHome.jsx
+++ layout/React-2025/projeto-spotfy/src/components/DisplayHome.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import NavBar from './NavBar'
-import { albumsData } from '../assets/frontend-assets/assets'
+import { albumsData, songsData } from '../assets/frontend-assets/assets'
 import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
+// import { songsData } from '../assets/frontend-assets/assets'
 
 const DisplayHome = () => {
   return (
     <>
     <NavBar />
-    <div className='my-5 font-bold text-wrap'>
-{albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+    <div className='mb-4'>
+        <h1 className='my-5 font-bold text-wrap'>Top 10</h1>
+        <div className='flex overflow-auto'>
+            {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+        </div>
+        
+    </div>
+
+    <div className='mb-4'>
+        <h1 className='my-5 font-bold text-wrap'>Melhores do Dia</h1>
+        <div className='flex overflow-auto'>
+            {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+        
     </div>
     </>
   )
