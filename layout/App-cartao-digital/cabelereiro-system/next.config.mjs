a8300b429f418d12dc344e92943d2b6155f0cfcc
LEONARDO AUGUSTO ALBUQUERQUE <96493737+LeoProStyle@users.noreply.github.com>
2025-05-12 10:17:23 -0300

    ajuste favicon

--- layout/App-cartao-digital/cabelereiro-system/next.config.mjs
+++ layout/App-cartao-digital/cabelereiro-system/next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.clerk.dev'],    
-  }  
- 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
-
 
 export default nextConfig;
